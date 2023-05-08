712e2c6214bbed9898952fbe3553c22b7fc2503a
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-05-08 20:53:09 +0200

    Settings: can modify path/folder names & paths

--- pydive/controllers/settings.py
+++ pydive/controllers/settings.py
@@ -56,6 +56,8 @@ class SettingsController:
         self.ui = {}
         self.ui["main"] = QtWidgets.QWidget()
         self.ui["layout"] = QtWidgets.QGridLayout()
+        self.ui["layout"].setColumnStretch(0, 1)
+        self.ui["layout"].setColumnStretch(2, 5)
 
         self.ui["main"].setLayout(self.ui["layout"])
         self.ui["layout"].setHorizontalSpacing(
@@ -66,33 +68,99 @@ class SettingsController:
 
         # Paths for data
         self.ui["paths"] = {}
-        for alias in self.database.storagelocations_get():
-            self.ui["paths"][alias.id] = {}
-            folder = self.ui["paths"][alias.id]
-            folder["button"] = PathSelectButton(alias.name, alias.type.name)
-            folder["display"] = QtWidgets.QLineEdit()
-            folder["model"] = alias
-
-            self.ui["layout"].addWidget(folder["button"], len(self.ui["paths"]), 0)
-            self.ui["layout"].addWidget(folder["display"], len(self.ui["paths"]), 1)
 
     @property
     def display_widget(self):
         """Returns the QtWidgets.QWidget for display of this screen"""
 
-        for alias, folder in self.ui["paths"].items():
-            folder["button"].target = folder["model"].path
-            folder["button"].pathSelected.connect(
-                lambda a, alias=alias: self.set_storagelocation(alias, a)
+        for location in self.database.storagelocations_get():
+            self.ui["paths"][location.id] = {}
+            folder = self.ui["paths"][location.id]
+            folder["model"] = location
+
+            # Path name
+            folder["name"] = QtWidgets.QWidget()
+            folder["name_layout"] = QtWidgets.QStackedLayout()
+
+            folder["name_label"] = QtWidgets.QLabel()
+            folder["name_label"].setText(folder["model"].name)
+
+            folder["name_edit"] = QtWidgets.QLineEdit()
+            folder["name_edit"].returnPressed.connect(
+                lambda: self.on_validate_name_change(location_id)
             )
-            folder["display"].setText(folder["model"].path)
-            folder["display"].setEnabled(False)
+
+            folder["name"].setLayout(folder["name_layout"])
+            folder["name_layout"].insertWidget(0, folder["name_label"])
+            folder["name_layout"].insertWidget(1, folder["name_edit"])
+
+            # Change path name
+            folder["name_change"] = QtWidgets.QWidget()
+            folder["name_change_layout"] = QtWidgets.QStackedLayout()
+            folder["name_change_start"] = QtWidgets.QPushButton(
+                QtGui.QIcon("assets/images/modify.png"), "", self.parent_window
+            )
+            folder["name_change_start"].clicked.connect(
+                lambda: self.on_click_name_change(location.id)
+            )
+            folder["name_change_end"] = QtWidgets.QPushButton(
+                QtGui.QIcon("assets/images/done.png"), "", self.parent_window
+            )
+            folder["name_change_end"].clicked.connect(
+                lambda: self.on_validate_name_change(location.id)
+            )
+            folder["name_change"].setLayout(folder["name_change_layout"])
+            folder["name_change_layout"].insertWidget(0, folder["name_change_start"])
+            folder["name_change_layout"].insertWidget(1, folder["name_change_end"])
+
+            # Actual path
+            folder["path"] = QtWidgets.QLineEdit()
+            folder["path"].setText(folder["model"].path)
+            folder["path"].setEnabled(False)
+
+            # Path change
+            folder["path_change"] = PathSelectButton(_("Change"), location.type.name)
+            folder["path_change"].target = folder["model"].path
+            folder["path_change"].pathSelected.connect(
+                lambda a, location=location: self.set_storagelocation(location.id, a)
+            )
+
+            # Delete button
+
+            self.ui["layout"].addWidget(folder["name"], len(self.ui["paths"]), 0)
+            self.ui["layout"].addWidget(folder["name_change"], len(self.ui["paths"]), 1)
+            self.ui["layout"].addWidget(folder["path"], len(self.ui["paths"]), 2)
+            self.ui["layout"].addWidget(folder["path_change"], len(self.ui["paths"]), 3)
         return self.ui["main"]
 
-    def set_storagelocation(self, alias_id, path):
-        self.ui["paths"][alias_id]["display"].setText(path)
-        self.ui["paths"][alias_id]["model"].path = path
-        self.database.session.add(self.ui["paths"][alias_id]["model"])
+    def on_click_name_change(self, location_id):
+        location = self.ui["paths"][location_id]
+        # Set value of edit field
+        location["name_edit"].setText(location["model"].name)
+
+        # Make widgets visible
+        location["name_layout"].setCurrentIndex(1)
+        location["name_change_layout"].setCurrentIndex(1)
+
+    def on_validate_name_change(self, location_id):
+        location = self.ui["paths"][location_id]
+        # Save the change
+        location["model"].name = location["name_edit"].text()
+        self.database.session.add(location["model"])
+        self.database.session.commit()
+
+        # Update label value
+        location["name_label"].setText(location["model"].name)
+
+        # Make widgets visible
+        location["name_layout"].setCurrentIndex(0)
+        location["name_change_layout"].setCurrentIndex(0)
+
+    def set_storagelocation(self, location_id, path):
+        location = self.ui["paths"][location_id]
+        location["path"].setText(path)
+        location["model"].path = path
+        self.database.session.add(location["model"])
         self.database.session.commit()
 
     @property
@@ -107,9 +175,8 @@ class SettingsController:
 
     def refresh_display(self):
         """Refreshes the display - update the paths displayed"""
-        # TODO: Implement settings refresh
-
         # Refresh list of paths
-        for alias, folder in self.ui["paths"].items():
-            folder["button"].target = folder["model"].path
-            folder["display"].setText(folder["model"].path)
+        for location, folder in self.ui["paths"].items():
+            folder["name"].setText(folder["model"].name)
+            folder["path"].setText(folder["model"].path)
+            folder["path_change"].target = folder["model"].path
