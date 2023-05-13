61ae80a9bbda2fc9b3911a2e8e740acaaba322fe
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-05-13 14:58:36 +0200

    Picture screen: now displays folders + list of images

--- pydive/controllers/pictures.py
+++ pydive/controllers/pictures.py
@@ -14,13 +14,12 @@ from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt
 
 import models.repository
-from controllers.widgets.pathselectbutton import PathSelectButton
 from controllers.widgets.basetreewidget import BaseTreeWidget
 
 _ = gettext.gettext
 
 
-class PicturesTree  (BaseTreeWidget):
+class PicturesTree(BaseTreeWidget):
     """Picture organization, selection & link to trips
 
     Attributes
@@ -55,36 +54,37 @@ class PicturesTree  (BaseTreeWidget):
             "size": 0.4,
             "alignment": Qt.AlignLeft,
         },
-        {
-            "name": _("SD Card"),
-            "size": 0.2,
-            "alignment": Qt.AlignCenter,
-        },
-        {
-            "name": _("Temporary storage"),
-            "size": 0.2,
-            "alignment": Qt.AlignCenter,
-        },
-        {
-            "name": _("Archives"),
-            "size": 0.2,
-            "alignment": Qt.AlignCenter,
-        },
     ]
 
-    def __init__(self, trips):
-        self.trips = trips
+    def __init__(self, parent_controller, repository):
+        super().__init__(parent_controller)
+        self.repository = repository
+
+    def set_folders(self, folders):
+        self.columns = [self.columns[0]]
+        for folder in folders:
+            self.columns.append(
+                {
+                    "name": folder.name,
+                    "size": 0.2,
+                    "alignment": Qt.AlignCenter,
+                    "path": folder.path,
+                    "model": folder,
+                }
+            )
+        self.setColumnCount(len(self.columns))
+        self.setHeaderLabels([_(col["name"]) for col in self.columns])
 
     def fill_tree(self):
         self.clear()
 
-        for trip, pictures in self.trips.items():
+        for trip, pictures in self.repository.trips.items():
             trip_widget = self.add_trip(trip)
             for picture in pictures:
                 self.add_picture(trip_widget, picture)
 
     def add_trip(self, trip):
-        data = [trip, '', '', '']
+        data = [trip]
         trip_widget = QtWidgets.QTreeWidgetItem(data)
         self.addTopLevelItem(trip_widget)
 
@@ -94,14 +94,17 @@ class PicturesTree  (BaseTreeWidget):
         return trip_widget
 
     def add_picture(self, trip_widget, picture):
-        data = [picture, '', '', '']
+        data = [picture.main_name]
+        for column in self.columns[1:]:
+            data.append("OK" if column["name"] == picture.location_type else "KO")
         picture_widget = QtWidgets.QTreeWidgetItem(data)
         self.addTopLevelItem(trip_widget)
         ######################
-        # TODO : Organize per trip AND per picture name
+        # TODO : Organize per trip THEN per picture name
         ######################
 
         trip_widget.addChild(picture_widget)
+
 
 class PicturesController:
     """Picture organization, selection & link to trips
@@ -128,8 +131,12 @@ class PicturesController:
     -------
     __init__ (parent_window)
         Stores reference to parent window & defines UI elements.
+    on_load_pictures
+        Refreshes the tree of images (left part)
+    refresh_folders
+        Reloads the paths displayed at the top left
     refresh_display
-        Reloads the paths displayed in the UI
+        Reloads the UI
     """
 
     name = _("Pictures")
@@ -144,52 +151,53 @@ class PicturesController:
         """
         self.parent_window = parent_window
         self.repository = models.repository.Repository()
+        self.database = parent_window.database
 
         self.ui = {}
-        self.ui['main'] = QtWidgets.QWidget()
-        self.ui['layout'] = QtWidgets.QHBoxLayout()
-
-        self.ui['main'].setLayout(self.ui['layout'])
+        self.ui["main"] = QtWidgets.QWidget()
+        self.ui["layout"] = QtWidgets.QHBoxLayout()
+        self.ui["main"].setLayout(self.ui["layout"])
 
         # Left part: folders + picture locations
-        self.ui['left'] = QtWidgets.QWidget()
-        self.ui['left_layout'] = QtWidgets.QVBoxLayout()
-        self.ui['left'].setLayout(self.ui['left_layout'])
-        self.ui['layout'].addWidget(self.ui['left'], 1)
+        self.ui["left"] = QtWidgets.QWidget()
+        self.ui["left_layout"] = QtWidgets.QVBoxLayout()
+        self.ui["left"].setLayout(self.ui["left_layout"])
+        self.ui["layout"].addWidget(self.ui["left"], 3)
 
-        self.ui['folders'] = {}
-        for path in ['SD Card', 'Temporary storage', 'Archive']:
-            self.ui['folders'][path] = PathSelectButton(path, "folder")
-            self.ui['left_layout'].addWidget(self.ui['folders'][path], len(self.ui['folders']))
-            #TODO: Display the correct paths (from database?) & remove the below testing
-            if path == "SD Card":
-                self.ui['folders'][path].target = "/home/PiratJack/Programmation/Python/PyDive/staging/SD/"
-            elif path == "Temporary storage":
-                self.ui['folders'][path].target = "/home/PiratJack/Programmation/Python/PyDive/staging/temporary/"
-            elif path == "Archive":
-                self.ui['folders'][path].target = "/home/PiratJack/Programmation/Python/PyDive/staging/archive/"
+        # Grid for the folders
+        self.ui["left_grid"] = QtWidgets.QWidget()
+        self.ui["left_grid_layout"] = QtWidgets.QGridLayout()
+        self.ui["left_grid"].setLayout(self.ui["left_grid_layout"])
+        self.ui["left_layout"].addWidget(self.ui["left_grid"])
 
+        self.ui["folders"] = {}
+
+        # Load button
+        self.ui["load_button"] = QtWidgets.QPushButton(_("Load pictures"))
+        self.ui["left_layout"].addWidget(self.ui["load_button"])
+
+        # Picture tree
+        self.ui["picture_tree"] = PicturesTree(self, self.repository)
+        self.ui["left_layout"].addWidget(self.ui["picture_tree"], 1)
 
         # Right part: choose picture to keep + tasks in progress
-        self.ui['right'] = QtWidgets.QWidget()
-        self.ui['right_layout'] = QtWidgets.QVBoxLayout()
-        self.ui['right'].setLayout(self.ui['right_layout'])
-        self.ui['layout'].addWidget(self.ui['right'], 1)
+        self.ui["right"] = QtWidgets.QWidget()
+        self.ui["right_layout"] = QtWidgets.QVBoxLayout()
+        self.ui["right"].setLayout(self.ui["right_layout"])
+        self.ui["layout"].addWidget(self.ui["right"], 5)
 
-        self.ui['load_button'] = QtWidgets.QPushButton(_("Load pictures"))
-        self.ui['load_button'].clicked.connect(self.on_load_pictures)
-        self.ui['right_layout'].addWidget(self.ui['load_button'])
-
-        # #self.images = []
-        # #for i in [0,1]:
-            # #pass
-            # #self.images[i] = QtWidgets.QPixmap()
-            # #self.right_column.layout.addWidget(self.images[i])
+        # TODO: display the images
+        # TODO: Allow to delete images
+        # TODO: Allow to transfer images between folders
+        # TODO: display loading status
 
     @property
     def display_widget(self):
         """Returns the QtWidgets.QWidget for display of this screen"""
-        return self.ui['main']
+
+        self.ui["load_button"].clicked.connect(self.on_load_pictures)
+
+        return self.ui["main"]
 
     @property
     def toolbar_button(self):
@@ -203,9 +211,50 @@ class PicturesController:
 
     def on_load_pictures(self):
         """User clicks 'load pictures' => reload the tree of pictures"""
-        self.repository.load_pictures({name: widget.target for name, widget in self.ui['folders'].items() if widget.target})
+        self.repository.load_pictures(
+            {folder.name: folder.path for folder in self.folders}
+        )
+
+        self.ui["picture_tree"].fill_tree()
+
+    def refresh_folders(self):
+        """Refreshes the list of folders from DB"""
+        # Load list from DB
+        self.folders = self.database.storagelocations_get_folders()
+
+        # Remove existing widgets
+        for folder in self.ui["folders"].values():
+            self.ui["left_grid_layout"].removeWidget(folder["label"])
+            self.ui["left_grid_layout"].removeWidget(folder["path"])
+
+            folder["label"] = None
+            folder["path"] = None
+            folder["model"] = None
+
+        # Generate new widgets
+        self.ui["folders"] = {}
+        for alias in self.folders:
+            self.ui["folders"][alias.id] = {}
+            folder = self.ui["folders"][alias.id]
+            folder["model"] = alias
+            folder["label"] = QtWidgets.QLabel()
+            folder["label"].setText(folder["model"].name)
+            folder["path"] = QtWidgets.QLineEdit()
+            folder["path"].setText(folder["model"].path)
+            folder["path"].setEnabled(False)
+
+            self.ui["left_grid_layout"].addWidget(
+                folder["label"], len(self.ui["folders"]) - 1, 0
+            )
+            self.ui["left_grid_layout"].addWidget(
+                folder["path"], len(self.ui["folders"]) - 1, 1
+            )
 
     def refresh_display(self):
         """Refreshes the display - update trips & pictures"""
-        #TODO: Implement pictures refresh
-        pass
+        # Refresh folder names
+        self.refresh_folders()
+
+        # Refresh image tree
+        self.ui["picture_tree"].set_folders(self.folders)
+        self.ui["picture_tree"].fill_tree()
