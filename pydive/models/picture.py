66abee0c9a81ddf3c2233385e970f0e0ace0e9b2
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-05-14 10:57:17 +0200

    Picture model: improved handling of paths & errors

--- pydive/models/picture.py
+++ pydive/models/picture.py
@@ -4,6 +4,14 @@ import gettext
 _ = gettext.gettext
 
 
+class StorageLocationCollision(ValueError):
+    """Raised when 2 storage location are in one another"""
+
+    def __init__(self, message, path):
+        super().__init__(message)
+        self.path = path
+
+
 class Picture:
     def __init__(self, storage_locations, path):
         self.path = path
@@ -15,11 +23,13 @@ class Picture:
         ]
         if len(storage_location) == 1:
             location_name, location_path = storage_location[0]
-            self.trip = os.path.dirname(path)[len(location_name) + 1 :]
+            self.trip = os.path.dirname(path)[len(location_path) :]
+            if self.trip.startswith("/"):
+                self.trip = self.trip[1:]
             self.location_name = location_name
             self.main_name = os.path.split(path)[-1]
         else:
-            print("recognition failed", path)
+            raise StorageLocationCollision("recognition failed", path)
 
     def __repr__(self):
         return (self.main_name, self.trip, self.location_name, self.path).__repr__()
