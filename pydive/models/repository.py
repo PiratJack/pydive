2a6af1cfddf706cfd763a9d44409cdf883b043bd
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-06-19 16:10:34 +0200

    Repository: added documentation

--- pydive/models/repository.py
+++ pydive/models/repository.py
@@ -1,3 +1,22 @@
+"""Allows to access database & file system through a generic interface
+
+Classes
+----------
+Repository
+    Allows to access database & file system through a generic interface
+
+ProcessGroup
+    A group of background running processes
+
+CopyProcess
+    A process to copy pictures from one location to another
+
+GenerateProcess
+    A process to generate pictures from raw pictures
+
+ProcessSignals
+    Defines signals when processes are completed or in error
+"""
 import os
 import shutil
 import gettext
@@ -7,37 +26,65 @@ from .picture import Picture as PictureModel
 from .picturegroup import PictureGroup
 
 _ = gettext.gettext
-# TODO: Repository > add documentation
 
 
 class Repository:
+    """Allows to access database & file system through a generic interface
+
+    Attributes
+    ----------
+    allowed_extensions : list of str
+        List of picture extensions that can be displayed
+
+    Methods
+    -------
+    __init__
+        Defines default attributes
+    load_pictures (folders)
+        Loads all pictures from the provided storage locations (folders)
+    read_folder (pictures, location_name, path)
+        Reads a given folder recursively to find pictures
+    add_picture (picture_group, location, path)
+        Adds a single picture to a given picture_group
+    remove_picture (picture_group, picture)
+        Removes (deletes) a single picture
+    copy_pictures (target_location, trip, picture_group, conversion_method)
+        Copies pictures between folders
+    generate_pictures (target_location, conversion_methods, trip, picture_group)
+        Generates pictures by converting between different formats
+
+    """
+
     allowed_extensions = [".cr2", ".jpg", ".jpeg"]
 
-    def __init__(self, storage_locations=None):
+    def __init__(self):
+        """Defines default attributes"""
         self.storage_locations = {}
         self.pictures = []
         self.picture_groups = []
         self.process_groups = []
 
-        if storage_locations:
-            self.storage_locations = storage_locations.copy()
-            self.load_pictures()
+    def load_pictures(self, storage_locations):
+        """Loads all pictures from the provided storage locations (folders)
 
-    def load_pictures(self, storage_locations=None):
+        Pictures are added in the corresponding picture_group
+
+        Parameters
+        ----------
+        storage_locations : dict of str
+            The storage locations, in form name:path
+        """
         # Find all pictures
         pictures = []
-        if storage_locations:
-            self.storage_locations |= storage_locations.copy()
-            self.pictures = []
-            self.picture_groups = []
+        self.storage_locations |= storage_locations.copy()
+        self.picture_groups = []
         for name in self.storage_locations:
             new_pictures = self.read_folder([], name, self.storage_locations[name])
             if new_pictures:
                 pictures += new_pictures
-        self.pictures = pictures
 
         # Then group them
-        picture_names = set(picture.name for picture in self.pictures)
+        picture_names = set(picture.name for picture in pictures)
         picture_names = sorted(picture_names)
         for picture_name in picture_names:
             matching_groups = [
@@ -45,8 +92,8 @@ class Repository:
                 for group in self.picture_groups
                 if picture_name.startswith(group.name)
             ]
-            pictures = [
-                picture for picture in self.pictures if picture.name == picture_name
+            matching_pictures = [
+                picture for picture in pictures if picture.name == picture_name
             ]
 
             if len(matching_groups) == 0:
@@ -57,10 +104,21 @@ class Repository:
                 # This is because groups are created in alphabetical order
                 # Therefore groups with shorter names are processed first
                 group = matching_groups[0]
-            for picture in pictures:
+            for picture in matching_pictures:
                 group.add_picture(picture)
 
     def read_folder(self, pictures, location_name, path):
+        """Reads a given folder recursively to find pictures
+
+        Parameters
+        ----------
+        pictures : list of Picture
+            The list in which to add new pictures
+        location_name : str
+            The name of the storage location
+        path : str
+            The path to explore (will do nothing if it's not a folder)
+        """
         if not os.path.isdir(path):
             return None
         for element in os.listdir(path):
@@ -78,15 +136,60 @@ class Repository:
         return pictures
 
     def add_picture(self, picture_group, location, path):
+        """Adds a single picture to a given picture_group
+
+        Note: this function does not create picture files
+
+        Parameters
+        ----------
+        picture_group : PictureGroup
+            The picture group in which to add the picture
+        location : StorageLocation
+            The storage location in which the picture is
+        path : str
+            The file path of the picture to add
+        """
         picture = PictureModel({location.name: location.path}, path)
         picture_group.add_picture(picture)
 
     def remove_picture(self, picture_group, picture):
+        """Removes (deletes) a single picture
+
+        Parameters
+        ----------
+        picture_group : PictureGroup
+            The picture group in which to add the picture
+        picture : Picture
+            The picture to delete
+        """
         picture_group.remove_picture(picture)
 
     def copy_pictures(
         self, target_location, trip=None, picture_group=None, conversion_method=None
     ):
+        """Copies pictures between folders
+
+        This function will create new picture files (by copying them)
+        Triggers self.add_picture once the copy is complete
+
+        Parameters
+        ----------
+        target_location : StorageLocation
+            The location in which pictures should be copied
+        trip : str
+            The trip to copy. Ignored if blank or if picture_group is provided.
+            Either trip or picture_group must be provided
+        picture_group : PictureGroup
+            The picture group to copy. Ignored if blank.
+            Either trip or picture_group must be provided
+        conversion_method : str
+            Copy only pictures with a specific conversion method. If blank, copies all pictures.
+
+        Returns
+        ----------
+        process_group : ProcessGroup
+            The group of background processes copying pictures
+        """
         # Determine all the picture groups to process
         if picture_group:
             picture_groups = [picture_group]
@@ -126,6 +229,29 @@ class Repository:
     def generate_pictures(
         self, target_location, conversion_methods, trip=None, picture_group=None
     ):
+        """Generates pictures by converting between different formats
+
+        This function will create new picture files (by converting from RAW pictures)
+        Triggers self.add_picture once the generation is complete
+
+        Parameters
+        ----------
+        target_location : StorageLocation
+            The location in which pictures should be copied
+        trip : str
+            The trip to copy. Ignored if blank or if picture_group is provided.
+            Either trip or picture_group must be provided
+        picture_group : PictureGroup
+            The picture group to copy. Ignored if blank.
+            Either trip or picture_group must be provided
+        conversion_method : str
+            Copy only pictures with a specific conversion method. If blank, copies all pictures.
+
+        Returns
+        ----------
+        process_group : ProcessGroup
+            The group of background processes generating pictures
+        """
         # Determine all the picture groups to process
         if picture_group:
             picture_groups = [picture_group]
@@ -157,6 +283,13 @@ class Repository:
         return process_group
 
     def __getattr__(self, attr):
+        """Calculates & returns missing attributes. Only works for trips attribute.
+
+        Returns
+        ----------
+        trips : dict trip:picture_group.name:PictureGroup
+            A dict representing all existing trips
+        """
         if attr == "trips":
             trips = {}
             for picture_group in self.picture_groups:
@@ -167,9 +300,36 @@ class Repository:
 
 
 class ProcessGroup(QtCore.QObject):
+    """A group of process that share common attributes
+
+    Methods
+    -------
+    __init__ (task_type, trip, target_location)
+        Stores basic information about the task group
+    add_task (source_picture, method)
+        Adds a new task to the group
+    task_done (task, path)
+        Marks a single task as in done & stopped. Triggers self.update_progress
+    task_error (task, error)
+        Marks a single task as in error & stopped. Triggers self.update_progress
+    update_progress
+        Updates the progress. Emits finishes signal once complete.
+    """
+
     finished = QtCore.pyqtSignal()
 
     def __init__(self, task_type, trip, target_location):
+        """Stores basic information about the task group
+
+        Parameters
+        ----------
+        task_type : "copy" or "generate"
+            Which task to perform
+        trip : str
+            The name of the trip of the picture to copy/generate
+        target_location : StorageLocation
+            The location in which to copy or generate
+        """
         super().__init__()
         if task_type not in ["copy", "generate"]:
             raise ValueError("Task type is invalid")
@@ -180,6 +340,15 @@ class ProcessGroup(QtCore.QObject):
         self.target_location = target_location
 
     def add_task(self, source_picture, method=None):
+        """Adds a new task to the group
+
+        Parameters
+        ----------
+        source_picture : Picture
+            The picture to copy or convert
+        method : ConversionMethod
+            The method to use for conversion. None for copies.
+        """
         if self.task_type == "copy":
             process = CopyProcess(self, source_picture)
         elif self.task_type == "generate":
@@ -195,16 +364,35 @@ class ProcessGroup(QtCore.QObject):
         return process
 
     def task_done(self, task, path):
+        """Marks a single task as in done & stopped. Triggers self.update_progress
+
+        Parameters
+        ----------
+        task : CopyProcess or GenerateProcess
+            The task in error
+        path : str
+            The path of the newly created image
+        """
         task["status"] = "Stopped"
         task["file_path"] = path
         self.update_progress()
 
     def task_error(self, task, error):
+        """Marks a single task as in error & stopped. Triggers self.update_progress
+
+        Parameters
+        ----------
+        task : CopyProcess or GenerateProcess
+            The task in error
+        error : str
+            The error message
+        """
         task["status"] = "Stopped"
         task["error"] = error
         self.update_progress()
 
     def update_progress(self):
+        """Updates the progress. Emits finishes signal once complete."""
         done = len([p for p in self.tasks if p["status"] == "Stopped"])
         self.progress = done / len(self.tasks)
         if done == len(self.tasks):
@@ -215,7 +403,26 @@ class ProcessGroup(QtCore.QObject):
 
 
 class CopyProcess(QtCore.QRunnable):
+    """A process to copy pictures between locations
+
+    Methods
+    -------
+    __init__ (task_group, source_picture)
+        Stores the required parameters for the copy
+    run (folders)
+        Runs the copy, after making sure it won't create issues
+    """
+
     def __init__(self, task_group, source_picture):
+        """Stores the required parameters for the copy
+
+        Parameters
+        ----------
+        task_group : ProcessGroup
+            The group of related task this belongs to
+        source_picture : Picture
+            The source picture to copy
+        """
         super().__init__()
         self.signals = ProcessSignals()
 
@@ -232,6 +439,11 @@ class CopyProcess(QtCore.QRunnable):
         }
 
     def run(self):
+        """Runs the copy, after making sure it won't create issues
+
+        Will emit error signal if target file already exists
+        Will emit finished signal once copy is complete
+        """
         # Check target doesn't exist already
         if os.path.exists(self.parameters["target_file"]):
             self.signals.error.emit(_("Target file already exists"))
@@ -244,7 +456,28 @@ class CopyProcess(QtCore.QRunnable):
 
 
 class GenerateProcess(QtCore.QRunnable):
+    """A process to generate pictures from raw pictures
+
+    Methods
+    -------
+    __init__ (task_group, source_picture, method)
+        Determines the actual system command to run
+    run (folders)
+        Runs the command, after making sure it won't create issues
+    """
+
     def __init__(self, task_group, source_picture, method):
+        """Determines the actual system command to run
+
+        Parameters
+        ----------
+        task_group : ProcessGroup
+            The group of related task this belongs to
+        source_picture : Picture
+            The RAW picture to convert
+        method : ConversionMethod
+            The method to convert the image
+        """
         super().__init__()
         self.signals = ProcessSignals()
 
@@ -276,6 +509,11 @@ class GenerateProcess(QtCore.QRunnable):
         self.parameters = parameters
 
     def run(self):
+        """Runs the command, after making sure it won't create issues
+
+        Will emit error signal if target file already exists
+        Will emit finished signal once process is complete
+        """
         # Check target doesn't exist already
         if os.path.exists(self.parameters["target_file"]):
             self.signals.error.emit(_("Target file already exists"))
@@ -289,5 +527,15 @@ class GenerateProcess(QtCore.QRunnable):
 
 
 class ProcessSignals(QtCore.QObject):
+    """Defines signals when processes are completed or in error
+
+    Attributes
+    ----------
+    finished : pyqtSignal
+        Emitted once a process has finished
+    error : pyqtSignal
+        Emitted once a process is in error
+    """
+
     finished = QtCore.pyqtSignal(str)
     error = QtCore.pyqtSignal(str)
