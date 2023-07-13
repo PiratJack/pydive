1a6ce08ce75f7e7da8dd117c5ae595c7dccd1b53
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-07-13 17:55:36 +0200

    Main is now based on a class for GUI testing

--- pydive/main.py
+++ pydive/main.py
@@ -13,35 +13,48 @@ import models.database
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# Define some constants
-LOCALE_FOLDER = "./locale"
-STYLESHEET_FILE = "./assets/style/app.css"
-# Define some constants
-DATABASE_FILE = "sandbox.sqlite"
-if "--real" in sys.argv:
-    os.makedirs(
-        platformdirs.user_data_dir("piratjack-pydive", "PiratJack"),
-        exist_ok=True,
-    )
-    DATABASE_FILE = (
-        platformdirs.user_data_dir("piratjack-pydive", "PiratJack") + "/prod.sqlite"
-    )
-    logger.setLevel(logging.ERROR)
 
-# Setup translation
-gettext.bindtextdomain("messages", LOCALE_FOLDER)
-gettext.translation("messages", localedir=LOCALE_FOLDER).install()
+class PyDive:
+    LOCALE_FOLDER = os.path.join(os.path.dirname(__file__), "locale")
+    STYLESHEET_FILE = "./assets/style/app.css"
+    DATABASE_FILE = "sandbox.sqlite"
 
-# Connect to database
-database = models.database.Database(DATABASE_FILE)
+    def __init__(self, database_file=None):
+        if database_file:
+            self.DATABASE_FILE = database_file
+        elif "--real" in sys.argv:
+            os.makedirs(
+                platformdirs.user_data_dir("piratjack-pydive", "PiratJack"),
+                exist_ok=True,
+            )
+            self.DATABASE_FILE = (
+                platformdirs.user_data_dir("piratjack-pydive", "PiratJack")
+                + "/prod.sqlite"
+            )
+            logger.setLevel(logging.ERROR)
+
+        # Setup translation
+        gettext.bindtextdomain("messages", self.LOCALE_FOLDER)
+        gettext.translation(
+            "messages", localedir=self.LOCALE_FOLDER, languages=["fr"]
+        ).install()
+
+        # Connect to database
+        self.database = models.database.Database(self.DATABASE_FILE)
+
+        # Change platform to avoid Wayland-related warning messages
+        os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+    def run(self):
+        app = PyQt5.QtWidgets.QApplication(sys.argv)
+        with open(self.STYLESHEET_FILE, "r", encoding="UTF-8") as stylesheet:
+            app.setStyleSheet(stylesheet.read())
+
+        window = controllers.mainwindow.MainWindow(self.database)
+        window.showMaximized()
+        app.exec_()
+
 
 if __name__ == "__main__":
-    # Change platform to avoid Wayland-related warning messages
-    os.environ["QT_QPA_PLATFORM"] = "xcb"
-    app = PyQt5.QtWidgets.QApplication(sys.argv)
-    with open(STYLESHEET_FILE, "r", encoding="UTF-8") as stylesheet:
-        app.setStyleSheet(stylesheet.read())
-
-    window = controllers.mainwindow.MainWindow(database)
-    window.showMaximized()
-    app.exec_()
+    app = PyDive()
+    app.run()
