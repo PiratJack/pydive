ff276b43f0218ca300424330d03e5f2c850bfc51
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-07-14 14:37:45 +0200

    Picture: improved cross-OS compatibility

--- pydive/models/picture.py
+++ pydive/models/picture.py
@@ -72,8 +72,8 @@ class Picture:
         if len(storage_location) == 1:
             location = storage_location[0]
             self.trip = os.path.dirname(path)[len(location.path) :]
-            if self.trip.startswith(os.sep):
-                self.trip = self.trip[1:]
+            if self.trip.startswith(os.path.sep):
+                self.trip = self.trip[len(os.path.sep) :]
             self.location = location
             self.name = os.path.basename(path).rsplit(".", 1)[-2]
             self.filename = os.path.basename(path)
