4ad74a279b1219461f0c90f1be96a902c4a745d0
PiratJack <90952042+PiratJack@users.noreply.github.com>
2023-07-30 11:51:46 +0200

    PathSelectButton: added translation + parent element

--- pydive/controllers/widgets/pathselectbutton.py
+++ pydive/controllers/widgets/pathselectbutton.py
@@ -9,8 +9,8 @@ _ = gettext.gettext
 class PathSelectButton(IconButton):
     pathSelected = QtCore.pyqtSignal(str)
 
-    def __init__(self, image, target_type="folder"):
-        super().__init__(image)
+    def __init__(self, image, parent=None, target_type="folder"):
+        super().__init__(image, "", parent)
         self.target_type = "folder" if target_type == "folder" else "file"
         self.setIcon(image)
         self.clicked.connect(self.choose_path)
@@ -21,9 +21,9 @@ class PathSelectButton(IconButton):
 
     def choose_path(self):
         if self.target_type == "folder":
-            self.target = self.dialog.getExistingDirectory(self, "Choose Directory")
+            self.target = self.dialog.getExistingDirectory(self, _("Choose Directory"))
         else:
-            self.target, _ = self.dialog.getOpenFileName(None, "Choose File")
+            self.target, _a = self.dialog.getOpenFileName(None, _("Choose File"))
         if self.target:
             self.pathSelected.emit(self.target)
             return self.target
